f4e078fed314ad49cc434c622ea8a69d265332a8
aelaraby6 <body1262005@gmail.com>
2026-01-29 22:54:05 +0200

    Refactor URL configuration to include static file serving and improve import statements

--- developers_fm/urls.py
+++ developers_fm/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, re_path,include
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
-from apps.core.views import *
+from apps.core import views
+from apps.core.views import landingPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +29,15 @@ urlpatterns = [
     path('auth/', include('apps.accounts.urls'))
 ]
 
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+
+# Error handlers
+handler400 = views.error_400
+handler403 = views.error_403
+handler404 = views.error_404
+handler500 = views.error_500
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
